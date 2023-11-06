5e76e2589434a7989a5ff865d23eee6c3bd3702e
sreenath seelam <seelamsreenath1@gmail.com>
2023-11-06 18:48:52 +0800

    removed unwanted vendor lines from calc.py file

--- practice/calc.py
+++ practice/calc.py
@@ -15,17 +15,15 @@ def calculate_percentage(number, percentage):
     return (number * percentage) / 100
 
 # Example usage:
-number = int(input("Enter the number: "))
-percentage = int(input("Enter the percentage: "))
+number = float(input("Enter the number: "))
+percentage = float(input("Enter the percentage: "))
 
 result = calculate_percentage(number, percentage)
 print(f"{percentage}% of {number} is: {result}")
 
 # Asking for an additional input
-#total = int(input(f"Enter an additional amount to add to {result}: "))
-#final_total = result + total
-#print(f"Total after adding {total}: {final_total}")
 
-Total = int(input(f"Enter an additional amount to add to {result} + "))
+
+Total = float(input(f"Enter an additional amount to add to {result} + "))
 final_amount = result + Total
 print(f"Total amount after adding {Total} is {final_amount}")
