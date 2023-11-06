ec97e365e9176a7736230f292e52968edc295d5f
sreenath seelam <seelamsreenath1@gmail.com>
2023-11-06 18:43:26 +0800

    modified calc.py file

--- practice/calc.py
+++ practice/calc.py
@@ -1,3 +1,16 @@
+'''def calculate_percentage(number, percentage):
+    return (number * percentage) / 100
+
+# Example usage:
+number = int(input("Enter the number: "))
+percentage = int(input("Enter the percentage: "))
+
+
+result = calculate_percentage(number, percentage)
+print(f"{percentage}% of {number} is: {result}")
+
+
+Total = int(input("Enter diff amount": + {result}))'''
 def calculate_percentage(number, percentage):
     return (number * percentage) / 100
 
@@ -8,3 +21,11 @@ percentage = int(input("Enter the percentage: "))
 result = calculate_percentage(number, percentage)
 print(f"{percentage}% of {number} is: {result}")
 
+# Asking for an additional input
+#total = int(input(f"Enter an additional amount to add to {result}: "))
+#final_total = result + total
+#print(f"Total after adding {total}: {final_total}")
+
+Total = int(input(f"Enter an additional amount to add to {result} + "))
+final_amount = result + Total
+print(f"Total amount after adding {Total} is {final_amount}")
